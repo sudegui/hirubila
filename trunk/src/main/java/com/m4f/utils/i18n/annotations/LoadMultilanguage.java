0d570e6f364cc029678d9c1685e678aeb49ed07c
eduardo.perrino@m4f.es <eduardo.perrino@m4f.es@694d2b16-15ee-00fa-9bde-d10b74900977>
2011-10-06 16:09:01 +0000

    Deleting olds files and folders.

--- trunk/src/main/java/com/m4f/utils/i18n/annotations/LoadMultilanguage.java
+++
@@ -1,8 +0,0 @@
-package com.m4f.utils.i18n.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface LoadMultilanguage {
-}
