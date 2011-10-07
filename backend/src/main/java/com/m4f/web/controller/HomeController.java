0888805bcd136b2d03b824099e649fbab89d4c5f
eduardo.perrino <eduardo.perrino@m4f.es>
2011-10-07 12:00:33 +0000

    moving classes to hirubila commons lib.

--- backend/src/main/java/com/m4f/web/controller/HomeController.java
+++ backend/src/main/java/com/m4f/web/controller/HomeController.java
@@ -1,25 +1,17 @@
 package com.m4f.web.controller;
 
-import java.io.IOException;
+
 import java.security.Principal;
 import java.util.List;
 import java.util.Locale;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import com.m4f.business.domain.InternalUser;
-import com.m4f.business.domain.MediationService;
 import com.m4f.business.domain.Provider;
-import com.m4f.utils.StackTraceUtil;
-import com.m4f.web.bind.form.MediatorForm;
 import com.m4f.business.service.exception.ContextNotActiveException;
 import com.m4f.business.service.exception.ServiceNotFoundException;
-import com.m4f.business.service.ifc.I18nCourseService;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/")
