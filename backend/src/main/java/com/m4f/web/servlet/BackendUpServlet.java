43ffc1a8f09e2295dbbca68479a76f9211b77976
eduardo.perrino <eduardo.perrino@m4f.es>
2011-10-10 15:50:47 +0000

    Deleting static files and jsp views.

--- backend/src/main/java/com/m4f/web/servlet/BackendUpServlet.java
+++ backend/src/main/java/com/m4f/web/servlet/BackendUpServlet.java
@@ -2,20 +2,15 @@ package com.m4f.web.servlet;
 
 import java.io.IOException;
 import java.util.logging.Logger;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 import com.google.appengine.api.backends.BackendService;
 import com.google.appengine.api.backends.BackendServiceFactory;
-import com.m4f.web.context.LazyContextLoaderListener;
 
 public class BackendUpServlet extends HttpServlet {
 	
-	private static final Logger LOGGER = Logger.getLogger(LazyContextLoaderListener.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(BackendUpServlet.class.getName());
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
  		throws IOException {
