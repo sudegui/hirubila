c23746b613c1d10cc2316127f300cf47302d77b1
eduardo.perrino@m4f.es <eduardo.perrino@m4f.es@694d2b16-15ee-00fa-9bde-d10b74900977>
2011-11-21 15:21:55 +0000

    Annotations added to MultilanguageInterceptor to load aspect automatically.

--- trunk/commons/src/main/java/com/m4f/utils/i18n/MultilanguageInterceptor.java
+++ trunk/commons/src/main/java/com/m4f/utils/i18n/MultilanguageInterceptor.java
@@ -9,10 +9,18 @@ import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.Collection;
 import java.lang.reflect.Field;
 import com.google.appengine.api.datastore.Text;
 import com.m4f.business.domain.BaseEntity;
+import com.m4f.utils.StackTraceUtil;
 import com.m4f.utils.beans.BeanManager;
 import com.m4f.utils.i18n.annotations.Multilanguage;
 import com.m4f.utils.i18n.annotations.MultilanguageEmbedded;
@@ -22,39 +30,48 @@ import com.m4f.utils.i18n.model.impl.JdoI18nEntry;
 import com.m4f.utils.i18n.service.ifc.I18nService;
 import com.google.appengine.api.datastore.Category;
 
+@Aspect
 public class MultilanguageInterceptor {
 	
 	private static final Logger LOGGER = Logger.getLogger(MultilanguageInterceptor.class.getName());
 	
+	@Autowired
 	private I18nService i18nService;
+	@Autowired
 	private BeanManager beanManager;
 	
+	public MultilanguageInterceptor(){}
 	
-	public MultilanguageInterceptor(I18nService i18n, BeanManager beanManager) {
+	public MultilanguageInterceptor(I18nService i18n, BeanManager bManager){
 		this.i18nService = i18n;
-		this.beanManager = beanManager;
+		this.beanManager = bManager;
 	}
-		
-		
-	public void loadCollection(List retVal, Locale locale, Object clazz) throws Throwable {
+	
+	@AfterReturning(
+		pointcut="execution(* com.m4f.utils.i18n.dao.ifc.I18nDAOSupport.saveOrUpdate*(..)) && args(entity,locale)",
+		argNames="entity,locale")
+	public <T extends I18nBehaviour> void putEntity(T entity, Locale locale) throws Throwable {
+		if(locale == null) {return;}
+		this.deleteTranslations(locale, entity);
+		this.locateMultiLanguageFields(locale, entity);
+	}
+	
+	
+	@Pointcut("(execution(* com.m4f.utils.i18n.dao.ifc.I18nDAOSupport.findById(..)) " +
+			"|| execution(* com.m4f.utils.i18n.dao.ifc.I18nDAOSupport.findEntity(..))) && args(clazz,locale,..)")	
+	public void findEntity(Object clazz, Locale locale){}
+	
+	
+	@AfterReturning(pointcut ="findEntity(clazz,locale)", argNames="clazz,locale,retVal", returning="retVal")
+	public void loadEntity(Object clazz, Locale locale, I18nBehaviour retVal) {
 		if(locale == null) {return;}
 		if(retVal == null) {return;}
-		this.loadCollection(locale, (List<I18nBehaviour>)retVal);
+		try {
+			this.loadObject(locale, retVal);
+		} catch(Exception e) {
+			LOGGER.severe(StackTraceUtil.getStackTrace(e));
+		}
 	}
-	
-	
-	public void loadEntity(Object clazz, I18nBehaviour retVal, 
-			Locale locale) throws Throwable {
-		if(locale == null) {return;}
-		if(retVal == null) {return;}
-		this.loadObject(locale, retVal);
-	}
-	
-	public <T extends I18nBehaviour> void deleteCollection(Collection<T> objs, 
-			Locale locale) throws Exception {
-		for(T object : objs) {this.deleteAllTranslations(object);}
-	}
-	
 	
 	/**
 	 * Intercepts methods that declare com.m4f.utils.i18n.annotations.DeleteMultilanguage 
@@ -65,45 +82,48 @@ public class MultilanguageInterceptor {
 	 * @throws Throwable in case something goes wrong in the actual method call
 	 */
 	@SuppressWarnings("unchecked")
-	public <T extends I18nBehaviour> Object deleteMultilanguage(ProceedingJoinPoint pjp) throws Throwable {
+	@Around("execution(* com.m4f.utils.i18n.dao.ifc.I18nDAOSupport.delete(..))")
+	public <T extends I18nBehaviour> Object deleteEntity(ProceedingJoinPoint pjp) throws Throwable {
 		T object = (T) pjp.getArgs()[0];
 		Object retVal = pjp.proceed();
 		this.deleteAllTranslations(object);
 		return retVal;
 	}
 	
-		
-	/**
-	 * Intercepts methods that declare com.m4f.utils.i18n.annotations.PutMultilanguage
-	 * and prints out the time it takes to complete.
-	 * 
-	 * @param pjp proceeding join point
-	 * @return the intercepted method returned object
-	 * @throws Throwable in case something goes wrong in the actual method call
-	 */
-	/*public Object putMultilanguage(ProceedingJoinPoint pjp) throws Throwable {
-		Object retVal = pjp.proceed();
-		Object object = pjp.getArgs()[0];
-		this.deleteTranslations(this.discoverLocale(pjp.getArgs()), (I18nBehaviour)object);
-		this.locateMultiLanguageFields(this.discoverLocale(pjp.getArgs()), object);
-		return retVal;
-	}*/
-	
-	
-	public <T extends I18nBehaviour> void putMultilanguage(T entity, Locale locale) throws Throwable {
-		if(locale == null) {return;}
-		this.deleteTranslations(locale, entity);
-		this.locateMultiLanguageFields(locale, entity);
-	}
-	
-	
-	public <T extends I18nBehaviour> void collection(Collection<T> entities, Locale locale) throws Throwable {
+	@AfterReturning(
+		pointcut ="execution (* com.m4f.utils.i18n.dao.ifc.I18nDAOSupport.saveOrUpdateCollection*(..)) " +
+				"and args(entities,locale)", argNames="entities,locale")
+	public <T extends I18nBehaviour> void putCollection(Collection<T> entities, Locale locale) throws Throwable {
 		if(locale == null) {return;}
 		for(T entity : entities) {
 			this.deleteTranslations(locale, entity);
 			this.locateMultiLanguageFields(locale, entity);	
 		}
 	}
+	
+	
+	@Pointcut("(execution(* com.m4f.utils.i18n.dao.ifc.I18nDAOSupport.findAll(..)) " +
+			"|| execution(* com.m4f.utils.i18n.dao.ifc.I18nDAOSupport.findEntities*(..))) && args(clazz,locale,..)")	
+	public void findCollection(Locale locale, Object clazz){}
+	
+	@AfterReturning(pointcut ="findCollection(locale,clazz)", argNames="locale,clazz,retVal", returning="retVal")
+	public void loadCollection(Locale locale, Object clazz, List retVal) throws Throwable {
+		if(locale == null) {return;}
+		if(retVal == null) {return;}
+		this.loadCollection(locale, (List<I18nBehaviour>)retVal);
+	}
+	
+	
+	@AfterReturning(
+			pointcut ="execution(* com.m4f.utils.i18n.dao.ifc.I18nDAOSupport.delete(java.util.Collection,java.util.Locale)) " +
+			"and args(objs,locale)", argNames="objs,locale")
+	public <T extends I18nBehaviour> void deleteCollection(Collection<T> objs, 
+			Locale locale) throws Exception {
+		for(T object : objs) {this.deleteAllTranslations(object);}
+	}
+		
+	
+	
 		
 	/****************************************************************************************
 	 * 
